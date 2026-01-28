d8f23cb0f299a5eda417111fcdd1776525979ca6
Devendra kumar <kumar.devendra1106@gmail.com>
2026-01-28 20:23:08 +0530

    ml starting
    only practice

---
+++ simple-program-practice/file.py
@@ -0,0 +1,52 @@
+# with open("sample.txt",'r') as f:
+#     d=f.read()
+
+#     new_d=d.replace("hello","Hii")
+#     # for i in d:
+#     #     print(i)
+#     print(new_d)
+
+
+# A = [
+#     [1, 2, 3],
+#     [4, 5, 6]
+# ]
+
+# B = [
+#     [7, 8, 9],
+#     [1, 2, 3]
+# ]
+
+
+# print(len(A[0]))
+
+
+# a=[1,2,3,4,5,1,2,3,3]
+# a.sort()
+# print(a)
+
+
+# result = []
+
+# for i in range(len(A)):
+#     row = []
+#     for j in range(len(A[0])):
+#         row.append(A[i][j] + B[i][j])
+#     result.append(row)
+
+# print("Sum of matrices:")
+# for r in result:
+#     print(r)
+
+
+# for i in range(5):
+#     if i == 3:
+#         continue
+#     print(i)
+
+lst = [1, 2, 3]
+it = iter(lst)
+
+# print(next(it))
+# print(next(it))
+print(next(it))
